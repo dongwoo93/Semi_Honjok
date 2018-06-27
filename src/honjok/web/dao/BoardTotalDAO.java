8f94ccce36bda68b0e68de9c3f1ce297d4bcb61e
DongWoo Lee <ehdrod12@gmail.com>
2018-06-27 15:48:01 +0900

    결제,쇼핑 검색 테스트 중

---
+++ src/honjok/web/dao/BoardTotalDAO.java
@@ -0,0 +1,20 @@
+package honjok.web.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import kh.web.dbutils.DBUtils;
+
+public class BoardTotalDAO {
+	public void selectData() throws Exception {
+		Connection con = DBUtils.getConnection();
+		String sql = "select * from (select board_tip.tip_title, row_number() over(order by tip_writedate desc) as num from board_tip) "
+				+ "where num between 1 and 5";
+		PreparedStatement pstat = con.prepareStatement(sql);
+		ResultSet rs = pstat.executeQuery();
+//		List<BoardDTO> list = new ArrayList<>();
+	}
+}
